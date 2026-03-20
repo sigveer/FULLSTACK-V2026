1c03c9433b07562f4169d2bc4300dcbf1ad0d582
sigveer <sigve.eriksen@gmail.com>
2026-03-20 16:10:14 +0100

    Initialize backend structure with Maven wrapper, configuration files, and basic Spring Boot application setup

---
+++ ik-system/backend/ik-common/src/main/java/com/iksystem/ik_common/IkCommonApplication.java
@@ -0,0 +1,13 @@
+package com.iksystem.ik_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkCommonApplication.class, args);
+	}
+
+}
---
+++ ik-system/backend/ik-common/src/test/java/com/iksystem/ik_common/IkCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iksystem.ik_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

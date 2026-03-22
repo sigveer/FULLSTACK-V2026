c7b7df26a20a3b4b77f65e752c3042f577d8d0f6
Piranavan Visvalingam <piranavan.visvalingam@outlook.com>
2026-03-22 17:21:23 +0100

    Start på login side

---
+++ frontend/src/views/login.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="login">
+    <div class="container">
+      <p class="step">STEG 1: INNLOGGING</p>
+
+      <div class="card">
+        <p class="cardtitle">IK-Komplett</p>
+        <p class="cardundertitle">Logg inn for å fortsette</p>
+
+        <label>E-post</label>
+
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>

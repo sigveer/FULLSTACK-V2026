bdfad389fd030f61e4590b1d91318dba68560eb6
Piranavan Visvalingam <piranavan.visvalingam@outlook.com>
2026-03-29 08:51:11 +0200

    Started on designing the createBusinessView.vue, without any css coding, but have sectioned it nicely for further designing.

---
+++ frontend/src/views/createBusinessView.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="card">
+      <h1 class="card-title">Opprett virksomhet</h1>
+      <p class="card-subtitle">Fyll inn informasjon om din virksomhet</p>
+
+      <div class="field">
+        <label for="businessName">Virksomhetsnavn</label>
+      </div>
+
+      <div class="field">
+        <label for="businessEmail">Virksomhetsmail</label>
+      </div>
+
+      <div class="field">
+        <label for="OrgNr">Org nmr/id</label>
+      </div>
+
+      <div class="field">
+        <label for="businessType">Type virksomhet</label>
+        <div class="select-wrap">
+          <select id="businsType">
+            <option>Aksjeselskap (AS)</option>
+            <option>Enkeltpersonforetak (ENK)</option>
+            <option>Ansvarlig selskap (ANS)</option>
+            <option>Samvirkeforetak (SA)</option>
+            <option>Stiftelse</option>
+            <option>Annet</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
